d650d10deb8038dc3732dcb3a7bdbfd211d80e86
--unset <bpriyan18082004@gmail.com>
2025-05-12 22:59:45 +0530

    fix: update sticker dimensions and styles in StickerModal component

--- frontend/src/components/stickerModal/stickerModal.jsx
+++ frontend/src/components/stickerModal/stickerModal.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef } from "react";
 import { Modal, Input, Button } from "antd";
 import { PrinterOutlined } from "@ant-design/icons";
 import dayjs from "dayjs";
@@ -28,35 +28,36 @@ const StickerModal = ({
 
     for (let i = 0; i < stickerCount; i++) {
       const clone = input.cloneNode(true);
-      clone.style.margin = "0";
+      clone.style.margin = "2";   //value changed from 0 to 2
       clone.style.display = "inline-block";
       stickerWrapper.appendChild(clone);
     }
 
-    const stickerWidth = 105;
-    const stickerHeight = 50;
+    // Set sticker dimensions in mm
+    const stickerWidth = 50; // mm (True-Ally size)
+    const stickerHeight = 25; // mm
     const stickersPerRow = 2;
 
-    const pdf = new jsPDF();
+    const pdf = new jsPDF({
+      unit: "mm",
+      format: "a4",
+    });
     const pdfWidth = pdf.internal.pageSize.getWidth();
     const pdfHeight = pdf.internal.pageSize.getHeight();
 
     for (let i = 0; i < stickerCount; i++) {
-        // Clone the sticker
       const singleStickerCanvas = await html2canvas(input, {
         useCORS: true,
         scale: 2,
       });
       const stickerImage = singleStickerCanvas.toDataURL("image/png");
 
-    //   Calculate position for each sticker
       const px = (i % stickersPerRow) * stickerWidth;
       const py =
         (Math.floor(i / stickersPerRow) %
           Math.floor(pdfHeight / stickerHeight)) *
         stickerHeight;
 
-        // New page if needed
       if (
         i > 0 &&
         i % (stickersPerRow * Math.floor(pdfHeight / stickerHeight)) === 0
@@ -84,7 +85,8 @@ const StickerModal = ({
           <div
             ref={previewRef}
             style={{
-              width: 200,
+              width: 189,
+              height: 94,
               padding: 7,
               textAlign: "center",
               border: "1px dashed gray",
@@ -96,45 +98,61 @@ const StickerModal = ({
             }}
           >
             <div className="flex flex-row gap-2 justify-center items-center">
-                <div className="flex-1 flex-col items-center justify-center gap-2">
-                  <img
-                    crossOrigin="anonymous"
-                    src={`${apiHost}/public/${product.qr_code}`}
-                    alt="QR"
-                    style={{ width: 50, height: 50 }}
-                  />
-                  <p className="font-bold">{product.product_code}</p>
-                </div>
-                <div>
-                  <b>Hunny Bunny🐝</b>
-                  <h4 className="font-bold">{product.product_name.toUpperCase()}</h4>
-                  <div className="flex flex-row-reverse gap-2 mt-1">
-                      <div className="flex-1 flex-col">
-                          <p className="font-semibold text-sm">MRP {product.product_price}</p>
-                          <p className="text-xs">{product.product_quantity} gm</p>
-                      </div>
-                      <div className="flex-1 flex-col ">
-                          <p className="font-semibold text-xs">
-                            pkd:{" "}
-                            {editStates[product.id]?.pkd
-                              ? dayjs(editStates[product.id].pkd, "DD-MM-YYYY").format("DD-MM-YYYY")
-                              : "--"}
-                          </p>
-                          <p className="font-semibold text-xs">
-                            exp:{" "}
-                            {editStates[product.id]?.exp
-                              ? dayjs(editStates[product.id].exp ,"DD-MM-YYYY").format("DD-MM-YYYY")
-                              : "--"}
-                          </p>
-                      </div>
+              <div className="flex-1 flex-col items-center justify-center gap-2">
+                <img
+                  crossOrigin="anonymous"
+                  src={`${apiHost}/public/${product.qr_code}`}
+                  alt="QR"
+                  style={{ width: 50, height: 50 }}
+                />
+                <p style={{ fontWeight: "bold", fontSize: "10px" }}>
+                  {product.product_code}
+                </p>
+              </div>
+              <div>
+                <p style={{ fontWeight: "bold", fontSize: "10px" }}>
+                  Hunny Bunny
+                </p>
+                <h4 style={{ fontWeight: "bold", fontSize: "10px" }}>
+                  {product.product_name.toUpperCase()}
+                </h4>
+                <div className="flex flex-row-reverse gap-4 mt-1">
+                  <div className="flex-1 flex-col">
+                    <p style={{ fontWeight: "600", fontSize: "9px" }}>
+                      MRP {product.product_price}
+                    </p>
+                    <p style={{ fontSize: "8px" }}>
+                      {product.product_quantity} gm
+                    </p>
+                  </div>
+                  <div className="flex flex-col align-middle">
+                    <p style={{ fontWeight: "400", fontSize: "8px" }}>
+                      pkd:{" "}
+                      {editStates[product.id]?.pkd
+                        ? dayjs(
+                            editStates[product.id].pkd,
+                            "DD-MM-YYYY"
+                          ).format("DD-MM-YYYY")
+                        : "--"}
+                    </p>
+                    <p style={{ fontWeight: "400", fontSize: "8px" }}>
+                      exp:{" "}
+                      {editStates[product.id]?.exp
+                        ? dayjs(
+                            editStates[product.id].exp,
+                            "DD-MM-YYYY"
+                          ).format("DD-MM-YYYY")
+                        : "--"}
+                    </p>
                   </div>
                 </div>
+              </div>
             </div>
           </div>
 
           <Input
             type="number"
-            min={1}
+            min={2}
             value={stickerCount}
             onChange={(e) => setStickerCount(Number(e.target.value))}
             placeholder="Enter number of stickers"
