0210e2d5f51b4fd65ba5c912f30cca02f47de6ed
Priyadarshan-B <bpriyan18082004@gmail.com>
2025-06-13 13:33:17 +0530

    modified products page

--- backend/src/routes/products/products_routes.js
+++ backend/src/routes/products/products_routes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const products = require("../../controllers/products/qr_products");
+const quantity = require('../../controllers/products/quantity')
 
 const storage = multer.memoryStorage(); 
 const upload = multer({ storage });
 
 router.post('/qr_products', upload.single('qr_image'), products.post_qr_products);
 router.get('/qr_products', products.get_qr_products)
+
+router.get('/qty',quantity.get_quantity)
 module.exports = router;
