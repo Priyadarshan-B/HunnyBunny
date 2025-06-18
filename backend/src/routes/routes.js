b9741b12c75b1a3cd53910e4a646371e7b0035d9
--unset <bpriyan18082004@gmail.com>
2025-06-18 13:59:33 +0530

    Add endpoint to fetch today's sold products and integrate dashboard routes

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,6 +1,7 @@
 const productsRoutes = require('./products/products_routes');
 const authRoutes = require('./auth/auth_routes');
 const billsRoutes = require('./bills/bill_routes')
+const dashboardRoutes = require('./dashboard/dashboard_routes');
 const { authenticateToken } = require('../middleware/auth')
 const express = require('express');
 const router = express.Router();
@@ -10,5 +11,6 @@ router.use('/auth', authRoutes);
 // router.use(authenticateToken);
 router.use('/products', productsRoutes);
 router.use('/bills',billsRoutes)
+router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
